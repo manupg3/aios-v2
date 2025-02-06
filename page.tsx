8a9962207a596834a708078d32c3a874d72641cc
PeñaManuel <man_uh_14@hotmail.com>
2025-02-06 18:54:40 -0300

    first commit

---
+++ page.tsx
@@ -0,0 +1,20 @@
+import CodeExample from "@/components/ui/CodeExample"
+import Cta from "
+/ui/Cta"
+import Features from "@/components/ui/Features"
+import { GlobalDatabase } from "@/components/ui/GlobalDatabase"
+import Hero from "@/components/ui/Hero"
+import LogoCloud from "@/components/ui/LogoCloud"
+
+export default function Home() {
+  return (
+    <main className="flex flex-col overflow-hidden">
+      <Hero />
+      <LogoCloud />
+      <GlobalDatabase />
+      <CodeExample />
+      <Features />
+      <Cta />
+    </main>
+  )
+}
