aeaeb9a95a8167bf99cc6bd4fd9286c53e3bb374
PeñaManuel <man_uh_14@hotmail.com>
2025-02-06 19:49:26 -0300

    fixes 2

--- Cta.tsx
+++ Cta.tsx
@@ -1,7 +1,7 @@
 "use client"
 import Balancer from "react-wrap-balancer"
-import { Button } from "../Button"
-import { Input } from "../Input"
+import { Button } from "./Button"
+import { Input } from "./Input"
 
 export default function Cta() {
   return (
