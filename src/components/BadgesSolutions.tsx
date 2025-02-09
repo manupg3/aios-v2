568ace6bf26ba581c49e867c44b30d17218f24a5
PeñaManuel <man_uh_14@hotmail.com>
2025-02-09 10:43:49 -0300

    add tabs - add texts - all cards

--- src/components/BadgesSolutions.tsx
+++ src/components/BadgesSolutions.tsx
@@ -59,18 +59,14 @@ export default function BadgeSolutions() {
   const cardContents: Record<string, React.ReactElement> = {
     simple: (
       <div className="p-8 bg-white shadow rounded">
-        {/* Título principal */}
         <span className="text-sm font-bold">Solución Simple</span>
         <h2 className="mt-2  bg-gradient-to-br from-gray-900 to-gray-800 bg-clip-text py-2 text-3xl font-bold tracking-tighter text-transparent dark:from-gray-50 dark:to-gray-300 sm:text-5xl">          Transforma tu presencia digital.
         </h2>
-        {/* Descripción principal */}
         <p className="mt-6 max-w-3xl text-lg leading-7 text-gray-600 dark:text-gray-400">
         Desarrollamos páginas clave en tiempo record, landings conversionales, 
         one pages estratégicas y sitios básicos con diseños optimizados para resultados inmediatos.
         </p>
-        {/* Área de dos columnas */}
         <div className="mt-4 flex gap-4">
-          {/* Columna izquierda (70%) */}
           <div className="container-col-left w-[70%] pr-4 bg-gray-50  shadow-2xl shadow-black/10">
             <h3 className="text-2xl font-semibold text-gray-950 dark:text-gray-50">
                 1 a 3 Páginas 
@@ -78,6 +74,7 @@ export default function BadgeSolutions() {
                 <span>Hasta 3 Paginas</span>
                 <span>Formularios</span>
                 <span>Integraciones</span>
+                <span>Panel Administrador</span>
                 </h3>
             <p className="mt-2 text-gray-500">
             Si quieres dar tus primeros pasos en el entorno digital o necesitas presencia en línea rápidamente, 
@@ -98,14 +95,21 @@ export default function BadgeSolutions() {
     ),
     empresarial: (
       <div className="p-8 bg-white shadow rounded">
-        <span className="text-sm font-bold">Solución Simple</span>
-        <h2 className="mt-2  bg-gradient-to-br from-gray-900 to-gray-800 bg-clip-text py-2 text-3xl font-bold tracking-tighter text-transparent dark:from-gray-50 dark:to-gray-300 sm:text-5xl">Solución Empresarial</h2>
+        <span className="text-sm font-bold">Solución Empresarial</span>
+        <h2 className="mt-2  bg-gradient-to-br from-gray-900 to-gray-800 bg-clip-text py-2 text-3xl font-bold tracking-tighter text-transparent dark:from-gray-50 dark:to-gray-300 sm:text-5xl">Lleva tu empresa al proximo nivel</h2>
         <p className="mt-6 max-w-3xl text-lg leading-7 text-gray-600 dark:text-gray-400">
-          Esta es la descripción principal de la solución empresarial.
-        </p>
+        Si tu empresa requiere una presencia digital avanzada con un diseño personalizado y escalable, nuestras Soluciones Web Empresariales ofrecen páginas e integraciones ilimitadas sin complicaciones.
+       </p>
         <div className="mt-4 flex gap-4">
           <div className="container-col-left w-[70%] pr-4 bg-gray-50  shadow-2xl shadow-black/10">
-            <h3 className="text-2xl font-semibold text-gray-950 dark:text-gray-50">Detalles Empresariales</h3>
+          <h3 className="text-2xl font-semibold text-gray-950 dark:text-gray-50">
+                Pack Empresarial Completo 
+                <span>Pagina Ilimitadas</span>
+                <span>Integraciones IA</span>
+                <span>Flujos de Contacto/Remarketing</span>
+                <span>Integraciones Ilimitadas</span>
+                <span>Panel Empresarial</span> 
+                </h3>
             <p className="mt-2 text-gray-500">
               Aquí se muestra una descripción detallada para la columna izquierda.
             </p>
@@ -115,7 +119,7 @@ export default function BadgeSolutions() {
           </div>
           <div className="w-[30%] container-col-right empresarial">
             <p className="text-gray-500">
-              Información adicional en la columna derecha.
+            Pensada para empresas y organizaciones, esta solución web ofrece un número ilimitado de páginas personalizadas (Inicio, Servicios, Equipo, entre otras), con un diseño profesional y optimizado para cualquier dispositivo, garantizando la máxima presencia.
             </p>
           </div>
         </div>
@@ -123,41 +127,55 @@ export default function BadgeSolutions() {
     ),
     ecommerce: (
       <div className="p-8 bg-white shadow rounded">
-        <span className="text-sm font-bold">Solución Simple</span> 
-        <h2 className="mt-2  bg-gradient-to-br from-gray-900 to-gray-800 bg-clip-text py-2 text-3xl font-bold tracking-tighter text-transparent dark:from-gray-50 dark:to-gray-300 sm:text-5xl">Solución E-Commerce</h2>
+        <span className="text-sm font-bold">Solución E-Commerce</span> 
+        <h2 className="mt-2  bg-gradient-to-br from-gray-900 to-gray-800 bg-clip-text py-2 text-3xl font-bold tracking-tighter text-transparent dark:from-gray-50 dark:to-gray-300 sm:text-5xl">Diseños y funcionalidades optimizadas</h2>
         <p className="mt-6 max-w-3xl text-lg leading-7 text-gray-600 dark:text-gray-400">
-          Esta es la descripción principal de la solución e-commerce.
+        Para nuevas tiendas y comercios existentes que busquen ampliar su presencia digital, nuestras Soluciones E-Commerce ofrecen una plataforma sólida y funcional, diseñada para un crecimiento continuo y libre de complicaciones.
         </p>
         <div className="mt-4 flex gap-4">
           <div className="container-col-left w-[70%] pr-4 bg-gray-50  shadow-2xl shadow-black/10">
-            <h3 className="text-2xl font-semibold text-gray-950 dark:text-gray-50">Detalles de E-Commerce</h3>
+          <h3 className="text-2xl font-semibold text-gray-950 dark:text-gray-50">
+                Todo En Uno 
+                <span>Paginas Ilimitadas</span>
+                <span>Medios De Envio</span>
+                <span>Medios De Pago</span>
+                <span>Multiples Potenciadores De Ventas</span>
+                <span>Integraciones IA</span>
+                </h3>
             <p className="mt-2 text-gray-500">
-              Aquí se muestra una descripción detallada para la columna izquierda.
-            </p>
+            Potencia tu negocio con nuestra solución Todo En Uno: disfruta de páginas ilimitadas, medios de envío y pago flexibles, y múltiples potenciadores de ventas. Además, integramos IA para análisis de datos y chatbots, optimizando la experiencia del cliente y maximizando tus resultados.
+             </p>
             <button className="btn-card-ecommerce mt-4 px-4 py-2 rounded">
             Cotizar Solucion
             </button>
           </div>
           <div className="w-[30%] container-col-right ecommerce">
             <p className="text-gray-500">
-              Información adicional en la columna derecha.
-            </p>
+            Ideal para quienes buscan una solución de comercio electrónico ágil y de alto desempeño, con un diseño profesional, adaptación móvil y funcionalidades IA para mejorar la atención al cliente y optimizar las ventas.
+           </p>
           </div>
         </div>
       </div>
     ),
     mantenimiento: (
       <div className="p-8 bg-white ">
-        <span className="text-sm font-bold">Solución Simple</span>    
-        <h2 className="mt-2  bg-gradient-to-br from-gray-900 to-gray-800 bg-clip-text py-2 text-3xl font-bold tracking-tighter text-transparent dark:from-gray-50 dark:to-gray-300 sm:text-5xl">Mantenimiento Integral</h2>
+        <span className="text-sm font-bold">Mantenimiento Integral</span>    
+        <h2 className="mt-2  bg-gradient-to-br from-gray-900 to-gray-800 bg-clip-text py-2 text-3xl font-bold tracking-tighter text-transparent dark:from-gray-50 dark:to-gray-300 sm:text-5xl">Tu web o aplicacion actualizado y optimizado</h2>
         <p className="mt-6 max-w-3xl text-lg leading-7 text-gray-600 dark:text-gray-400">
-          Esta es la descripción principal del mantenimiento integral.
+        Ofrecemos mantenimiento integral para sitios web, e-commerce y aplicaciones, garantizando un óptimo rendimiento, actualizaciones continuas y la seguridad necesaria para que tu negocio se mantenga siempre a la vanguardia.
         </p>
         <div className="mt-4 flex gap-4">
           <div className="container-col-left w-[70%] pr-4 bg-gray-50  shadow-2xl shadow-black/10">
-            <h3 className="text-2xl font-semibold text-gray-950 dark:text-gray-50">Detalles del Mantenimiento</h3>
+          <h3 className="text-2xl font-semibold text-gray-950 dark:text-gray-50">
+                 Actualizado y Optmizado
+                <span>Landings</span>
+                <span>Sitios Empresariales</span>
+                <span>E-Commerce</span>
+                <span>A Medida</span>
+                <span>Multiples Plataformas</span>
+                </h3>
             <p className="mt-4 max-w-sm text-gray-700 dark:text-gray-400">
-              Aquí se muestra una descripción detallada para la columna izquierda.
+            Mantén cada plataforma actualizada y optimizada para asegurar rendimiento de primer nivel. Desde landings de alto impacto hasta sitios empresariales, e-commerce y soluciones a medida, garantizamos una experiencia fluida y resultados tangibles para tu negocio.
             </p>
             <button className="btn-card-mantenimiento mt-4 px-4 py-2 rounded">
             Cotizar Mantenimiento
@@ -165,24 +183,30 @@ export default function BadgeSolutions() {
           </div>
           <div className="w-[30%] container-col-right mantenimiento">
             <p className="mt-4 max-w-sm text-gray-700 dark:text-gray-400">
-              Información adicional en la columna derecha.
-            </p>
+            Ideal para negocios de cualquier tamaño, nuestro servicio de mantenimiento garantiza un rendimiento estable y una adaptación óptima para dispositivos móviles, manteniendo tu sitio siempre actualizado, optimizado y seguro.
+           </p>
           </div>
         </div>
       </div>
     ),
     ia: (
       <div className="p-8 bg-white shadow rounded">
-        <span className="text-sm font-bold">Solución Simple</span>      
-        <h2 className="mt-2  bg-gradient-to-br from-gray-900 to-gray-800 bg-clip-text py-2 text-3xl font-bold tracking-tighter text-transparent dark:from-gray-50 dark:to-gray-300 sm:text-5xl">Solución IA</h2>
+        <span className="text-sm font-bold">Solución IA</span>      
+        <h2 className="mt-2  bg-gradient-to-br from-gray-900 to-gray-800 bg-clip-text py-2 text-3xl font-bold tracking-tighter text-transparent dark:from-gray-50 dark:to-gray-300 sm:text-5xl">Revoluciona tu marca con IA</h2>
         <p className="mt-6 max-w-3xl text-lg leading-7 text-gray-600 dark:text-gray-400">
-          Esta es la descripción principal de la solución de IA.
+        Ofrecemos soluciones de Inteligencia Artificial que integran chatbots para una atención automatizada y eficiente, junto con un analizador de ventas que te permite procesar datos en tiempo real, optimizar estrategias y tomar decisiones informadas para impulsar el crecimiento de tu negocio.
         </p>
         <div className="mt-4 flex gap-4">
           <div className="container-col-left w-[70%] pr-4 bg-gray-50  shadow-2xl shadow-black/10">
-            <h3 className="text-2xl font-semibold text-gray-950 dark:text-gray-50">Detalles de la Solución IA</h3>
+          <h3 className="text-2xl font-semibold text-gray-950 dark:text-gray-50">
+                Chatbots, Analizadores y Mucho Mas 
+                <span>Chatbots</span>
+                <span>Analizadores De Ventas</span>
+                <span>Integracion ChatGpt - DeepSeek</span>
+                <span>Configuracion Simple</span>
+                </h3>
             <p className="mt-4 max-w-sm text-gray-700 dark:text-gray-400">
-              Aquí se muestra una descripción detallada para la columna izquierda.
+            Maximiza la eficiencia con chatbots inteligentes, analizadores de ventas y la integración de ChatGPT–DeepSeek. Nuestra configuración simple agiliza la implementación y potencia los resultados de tu negocio.
             </p>
             <button className="btn-card-ia mt-4 px-4 py-2 rounded w-max">
             Cotizar Solucion
@@ -190,7 +214,7 @@ export default function BadgeSolutions() {
           </div>
           <div className="w-[30%] container-col-right ia">
             <p className="mt-4 max-w-sm text-gray-700 dark:text-gray-400">
-              Información adicional en la columna derecha.
+            Ideal para quienes desean integrar Inteligencia Artificial de forma rápida y eficaz, nuestras soluciones se crean totalmente a medida, con un diseño profesional y tecnología de vanguardia. Creamos implementaciones personalizadas y escalables, facilitando la toma de decisiones y potenciando la competitividad de tu negocio.
             </p>
           </div>
         </div>
