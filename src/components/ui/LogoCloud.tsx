22b9fa8cb7cecbdc029a6f3f16cd2d8b30addb2c
PeñaManuel <man_uh_14@hotmail.com>
2025-02-11 20:38:08 -0300

    change imgs logos width

--- src/components/ui/LogoCloud.tsx
+++ src/components/ui/LogoCloud.tsx
@@ -9,31 +9,31 @@ export default function LogoCloud() {
       <p className="text-lg font-medium tracking-tighter text-gray-800 dark:text-gray-200">
         Trusted by the world&rsquo;s best engineering teams
       </p>
-      <div className="grid grid-cols-2 gap-10 gap-y-4 text-gray-900 md:grid-cols-5 md:gap-x-20 dark:text-gray-200">
+      <div className="container-imgs-stack grid grid-cols-2 gap-10 gap-y-4 text-gray-900 md:grid-cols-5 md:gap-x-20 dark:text-gray-200">
         <img 
           src="/images/wordpress-logo.svg" 
           alt="wordpress" 
-          className="w-28 h-auto"
+          className="h-auto"
         />
         <img 
           src="/images/shopify-logo.svg" 
           alt="shopify" 
-          className="w-28 h-auto"
+          className="h-auto"
         />
         <img 
           src="/images/react-logo.svg" 
           alt="Capsule" 
-          className="w-28 h-auto"
+          className="h-auto"
         />
         <img 
           src="/images/openai-logo.svg" 
           alt="Catalog" 
-          className="w-28 h-auto"
+          className="h-auto"
         />
         <img 
           src="/images/nextjs-logo.svg" 
           alt="Cloudwatch" 
-          className="w-28 h-auto"
+          className="h-auto"
         />
         
       </div>
