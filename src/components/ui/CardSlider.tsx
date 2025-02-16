aaba8e6b32ca91ab8ac699cdfe8817d4d9f3292a
PeñaManuel <man_uh_14@hotmail.com>
2025-02-16 19:53:33 -0300

    add cardslider

--- src/components/ui/CardSlider.tsx
+++ src/components/ui/CardSlider.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
-import styles from './CardSlider.module.css';
+import styles from '../../CardSlider.module.css'
 
 const CardSlider = () => {
   const settings = {
