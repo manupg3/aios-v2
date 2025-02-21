b3aa609425e355f84e5ce14bb26efe509a699393
PeñaManuel <man_uh_14@hotmail.com>
2025-02-21 09:23:58 -0300

    change asesoramiento carousel

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,9 +1,8 @@
 import { Badge } from "@/components/Badge"
-import { Button } from "@/components/Button"
 import Benefits from "@/components/ui/Benefits"
-import { cx } from "@/lib/utils"
 import Balancer from "react-wrap-balancer"
 import CardCarousel from "@/components/ui/CardCarousel"
+import Cta from "@/components/ui/Cta"
 
 export default function About() {
   return (
@@ -29,42 +28,11 @@ export default function About() {
         Optimizamos tu presencia en línea con estrategias web personalizadas para mejorar la funcionalidad y rendimiento de tu sitio, enfocándonos en aspectos clave como SEO y UX para maximizar el rendimiento 
         </p>
       </section>
+      <section className="section-card-carousel">
       <CardCarousel />
-      <Benefits />
-      <section aria-labelledby="vision-title" className="mx-auto mt-40">
-        <h2
-          id="vision-title"
-          className="inline-block bg-gradient-to-t from-gray-900 to-gray-800 bg-clip-text py-2 text-4xl font-bold tracking-tighter text-transparent md:text-5xl dark:from-gray-50 dark:to-gray-300"
-        >
-          Nuestra Vision
-        </h2>
-        <div className="mt-6 max-w-prose space-y-4 text-gray-600 dark:text-gray-400">
-          <p className="text-lg leading-8">
-            We envision a world where data management is no longer a complex
-            challenge but a powerful advantage. By integrating cutting-edge AI
-            into database solutions, we aim to transform raw data into strategic
-            assets, empowering businesses to innovate faster and more
-            efficiently.
-          </p>
-          <p className="text-lg leading-8">
-            We believe in removing the barriers of data complexity and
-            scalability, enabling teams to focus on insights and innovations
-            rather than maintenance and management. Our goal is to equip every
-            organization with the tools they need to harness the full potential
-            of their data, driving growth and excellence in every interaction.
-          </p>
-          <p
-            className={cx(
-              "w-fit rotate-3 font-handwriting text-3xl text-indigo-600 dark:text-indigo-400",
-            )}
-          >
-            – Alex and Robin
-          </p>
-        </div>
-        <Button className="mt-32 h-10 w-full shadow-xl shadow-indigo-500/20">
-          View Open Roles
-        </Button>
       </section>
+      <Benefits />
+      <Cta/>
     </div>
   )
 }
