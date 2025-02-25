22f5cb1bf7e43304541f496ec05865e19df2d934
PeñaManuel <man_uh_14@hotmail.com>
2025-02-25 19:19:04 -0300

    add transition framer - fix

--- src/app/ClientLayout.tsx
+++ src/app/ClientLayout.tsx
@@ -1,18 +1,14 @@
-// clientLayout.tsx
-"use client";
-
-import React, { useState } from "react";
-import { Navigation } from "@/components/ui/Navbar";
-import Footer from "@/components/ui/Footer";
-import { ThemeProvider } from "next-themes";
-import { Modal } from "@/components/ui/ModalLayout";
-import FormCotizacion from "@/components/ui/formQuotes";
+import React, { useRef, useEffect } from "react";
 import { AnimatePresence, motion } from "framer-motion";
 import { usePathname } from "next/navigation";
 
 export function ClientLayout({ children }: { children: React.ReactNode }) {
-  const [modalOpen, setModalOpen] = useState(false);
   const pathname = usePathname();
+  const isInitialMount = useRef(true);
+
+  useEffect(() => {
+    isInitialMount.current = false;
+  }, []);
 
   const variants = {
     hidden: { opacity: 0, x: 50 },
@@ -21,24 +17,17 @@ export function ClientLayout({ children }: { children: React.ReactNode }) {
   };
 
   return (
-    <ThemeProvider attribute="class" defaultTheme="system" disableTransitionOnChange>
-      <Navigation setModalOpen={setModalOpen} />
-      <AnimatePresence mode="wait">
-        <motion.div
-          key={pathname}
-          initial="hidden"
-          animate="enter"
-          exit="exit"
-          variants={variants}
-          transition={{ duration: 0.5 }}
-        >
-          {children}
-        </motion.div>
-      </AnimatePresence>
-      <Footer />
-      <Modal isOpen={modalOpen} onClose={() => setModalOpen(false)}>
-        <FormCotizacion />
-      </Modal>
-    </ThemeProvider>
+    <AnimatePresence mode="wait">
+      <motion.div
+        key={pathname}
+        initial={isInitialMount.current ? false : "hidden"}
+        animate="enter"
+        exit="exit"
+        variants={variants}
+        transition={{ duration: 0.5 }}
+      >
+        {children}
+      </motion.div>
+    </AnimatePresence>
   );
 }
