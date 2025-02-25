2095d5fbae41dacab3d1518e16bc40e541a72e20
PeñaManuel <man_uh_14@hotmail.com>
2025-02-25 19:39:05 -0300

    add transition framer - fix 4

--- src/app/ClientLayout.tsx
+++ src/app/ClientLayout.tsx
@@ -20,11 +20,6 @@ export function ClientLayout({ children }: { children: React.ReactNode }) {
   }, []);
 
 
-  const variants = {
-    hidden: { opacity: 0, x: 50 },
-    enter: { opacity: 1, x: 0 },
-    exit: { opacity: 0, x: -50 },
-  };
 
   return (
     <ThemeProvider attribute="class" defaultTheme="system" disableTransitionOnChange>
