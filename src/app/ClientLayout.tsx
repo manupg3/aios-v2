ef7ed84a3349f429f41041dc04caafa3b1f4e2d6
PeñaManuel <man_uh_14@hotmail.com>
2025-02-25 19:06:22 -0300

    add transition framer

--- src/app/ClientLayout.tsx
+++ src/app/ClientLayout.tsx
@@ -1,20 +1,40 @@
+// clientLayout.tsx
 "use client";
 
-import React, { useState } from 'react';
-import { Navigation } from '@/components/ui/Navbar';
-import Footer from '@/components/ui/Footer';
-import { ThemeProvider } from 'next-themes';
-import { Modal } from '@/components/ui/ModalLayout';
-import FormCotizacion from '@/components/ui/formQuotes';
+import React, { useState } from "react";
+import { Navigation } from "@/components/ui/Navbar";
+import Footer from "@/components/ui/Footer";
+import { ThemeProvider } from "next-themes";
+import { Modal } from "@/components/ui/ModalLayout";
+import FormCotizacion from "@/components/ui/formQuotes";
+import { AnimatePresence, motion } from "framer-motion";
+import { usePathname } from "next/navigation";
 
 export function ClientLayout({ children }: { children: React.ReactNode }) {
   const [modalOpen, setModalOpen] = useState(false);
+  const pathname = usePathname();
+
+  const variants = {
+    hidden: { opacity: 0, x: 50 },
+    enter: { opacity: 1, x: 0 },
+    exit: { opacity: 0, x: -50 },
+  };
 
   return (
     <ThemeProvider attribute="class" defaultTheme="system" disableTransitionOnChange>
-
-      <Navigation setModalOpen={setModalOpen} /> {/* Asegúrate de pasar setModalOpen aquí */}
-      {children}
+      <Navigation setModalOpen={setModalOpen} />
+      <AnimatePresence mode="wait">
+        <motion.div
+          key={pathname}
+          initial="hidden"
+          animate="enter"
+          exit="exit"
+          variants={variants}
+          transition={{ duration: 0.5 }}
+        >
+          {children}
+        </motion.div>
+      </AnimatePresence>
       <Footer />
       <Modal isOpen={modalOpen} onClose={() => setModalOpen(false)}>
         <FormCotizacion />
