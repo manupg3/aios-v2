75462b142a02c66873706aee19862d70ea8f961f
PeñaManuel <man_uh_14@hotmail.com>
2025-03-08 16:42:17 -0300

    contact-specialist tabs

--- src/components/ui/VerticalTabs.tsx
+++ src/components/ui/VerticalTabs.tsx
@@ -19,7 +19,7 @@ const tabsData = [
       </div>
 
       <div className='flex justify-center py-6 gap-2'>
-          <span className='font-semibold'>¿Necesitas un admin personalizado?</span> <a className='text-indigo-600 font-bold' href=''>contacta a un especialista</a>
+          <span className='font-medium'>¿Necesitas un admin personalizado?</span> <a className='text-indigo-600 font-bold' href=''>contacta a un especialista</a>
      </div>
       
 </div>
