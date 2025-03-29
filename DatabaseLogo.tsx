2d889880795348a47868f1bf5b2c6760ae3aad06
PeñaManuel <man_uh_14@hotmail.com>
2025-03-28 22:56:55 -0300

    change-logo

--- DatabaseLogo.tsx
+++ DatabaseLogo.tsx
@@ -2,5 +2,5 @@ import * as React from "react";
 import type { ImgHTMLAttributes } from "react";
 
 export const DatabaseLogo = (props: ImgHTMLAttributes<HTMLImageElement>) => (
-  <img src="/images/aios-logo.png" alt="aios Logo" {...props} />
+  <img src="/images/aios-logo.svg" alt="aios Logo" {...props} />
 );
